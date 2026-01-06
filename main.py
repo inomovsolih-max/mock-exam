bf7c8ea14ab4f47946679fb35d8423bd9ab29ae0
inomovsolih-max <inomovsolih@gmail.com>
2026-01-06 14:30:55 +0400

    jonatish

---
+++ main.py
@@ -0,0 +1,17 @@
+def sort_keys_by_value(my_dict):
+    saralangan = sorted(my_dict.items(), key=lambda x: x[1])
+    return [kalit for kalit, _ in saralangan]
+
+
+
+my_dict = {
+    "t": 3,
+    "p": 1,
+    "y": 2,
+    "o": 5,
+    "h": 4,
+    "n": 6,
+}
+
+for k in sort_keys_by_value(my_dict): 
+    print(k)
